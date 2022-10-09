6f49a52733adeb80849ab1dbe23dfb57b8181e5e
mark3010 <marklarsen96@gmail.com>
2022-10-09 11:18:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./public/frontpage.html"));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Listening on port 8080")
+})
