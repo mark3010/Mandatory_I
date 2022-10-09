db40be43ca23a2651cf22dc820da1ef92afa47dc
mark3010 <marklarsen96@gmail.com>
2022-10-09 11:25:29 +0200

    added page structure

---
+++ public/layout/footer/footer.js
@@ -0,0 +1,2 @@
+const copyrightMessage = `© ${new Date().getFullYear()}`;
+document.getElementById("copyright-year").innerText = "A Mark Friis Larsen website. " + copyrightMessage + " All rights reserved.";
